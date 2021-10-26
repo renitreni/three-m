370625086e52f330673e94a2f5a274185346b571
rtrenuela <rtrenuela@objectbright.com>
2021-10-26 23:08:23 +0800

    update

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -70,11 +70,10 @@
                     </form>
                 </div>
                 <h2 class="footer-heading mb-4">Follow Us</h2>
-                <a href="#about-section" class="smoothscroll pl-0 pr-3"><span class="icon-facebook"></span></a>
+                <a href="#" class="smoothscroll pl-0 pr-3"><span class="icon-facebook"></span></a>
                 <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
                 <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
                 <a href="#" class="pl-3 pr-3"><span class="icon-snapchat"></span></a>
-                <a href="#" class="pl-3 pr-3"><span class="icon-facebook"></span></a>
                 </form>
             </div>
         </div>
